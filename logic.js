050746a49d50f71e40c9f6579191f644c95eb45d
GiadaChiar <132461029+GiadaChiar@users.noreply.github.com>
2025-11-24 16:26:00 +0100

    logic table

---
+++ logic.js
@@ -0,0 +1,8 @@
+//functon to click on button and go to second page 
+buttonShowMore = document.getElementById("show_more");
+
+buttonShowMore.addEventListener("click", () => {
+
+    window.location.href = "search.html"; 
+
+})
---
+++ logic_second.js
@@ -0,0 +1,88 @@
+const vitB = document.getElementById("vitB");
+const subB = document.getElementById("subB");
+
+vitB.addEventListener("click", (e) => {
+    console.log("Bottone B cliccato")
+    e.preventDefault(); // impedisce di navigare
+    subB.classList.toggle("show");
+});
+
+
+//legend for each vitamins
+//take write to start to show table
+const navWrite=document.getElementById("vitamine");
+//find id to collocate the table
+const tableDiv= document.getElementById("tableDiv");
+
+//now logic if I click create table, show me table 
+
+navWrite.addEventListener("click", ()=>{
+    if(navWrite.textContent == "Le vitamine+"){
+        
+        //create table 
+    const Mytable= document.createElement("table");
+    Mytable.id="table_1"
+    Mytable.innerHTML = `
+            <thead>
+                <tr><td>Vitamina</td><td>Descrizione</td><td>Nutrienti</td></tr>
+            </thead>
+            <tbody>
+                <tr><td>A(Retinolo/Beta-carotene)</td><td>Ricca nei vegetali arancioni e gialli.</td><td>carote, zucca, albicocche, patate dolci, melone..</td></tr>
+                <tr><td>B</td><td>Spesso associate a cereali integrali e legumi.</td><td><ul>
+                <li>B1 (tiamina): cereali integrali, legumi</li>
+                <li>B2 (riboflavina): latte, mandorle</li>
+                <li>B3 (niacina): carne, arachidi</li>
+                <li>B5 (acido pantotenico): funghi, avocado</li>
+                <li>B6 (piridossina): banane, patate, cereali</li>
+                <li>B9 (Acido Folico): spinaci, asparagi, broccoli, avocado, legumi.</li>
+                <li>B12 : uova, latte, formaggi, carne, pesce.</li>
+                </ul></td></tr>
+                <tr><td>C</td><td>Presente in molti agrumi e frutti arancioni.</td><td>carote, zucca, albicocche, patate dolci, melone.</td></tr>
+                <tr><td>D</td><td>Si trova in alimenti marini e viene associata alla luce del sole</td><td>salmone, sgombro, uova, funghi esposti al sole.</td></tr>
+                <tr><td>E</td><td>Contenuta in oli vegetali e semi.</td><td>olio di girasole, mandorle, nocciole, semi.</td></tr>
+                <tr><td>K</td><td>Abbondante nelle verdure a foglia.</td><td>spinaci, cavolo nero, lattuga, broccoli, bietole.</td></tr>
+            </tbody>
+            `;
+
+            //create button delete x
+            const deleteBt=document.createElement("button");
+            deleteBt.id= "delete_bt"
+            deleteBt.id = "delete_bt";
+            deleteBt.type = "button";
+            deleteBt.classList.add("btn-close");
+            deleteBt.setAttribute("aria-label", "Close");
+
+            //if i click on the button delete table
+            deleteBt.addEventListener("click",()=>{
+                //delete table
+            const existingTable = document.getElementById("table_1")
+                if(existingTable){
+                    existingTable.remove()
+                }
+                deleteBt.remove();
+                navWrite.textContent="Le vitamine+";
+            })
+
+            //append table to its div
+            tableDiv.appendChild(Mytable);
+            Mytable.appendChild(deleteBt);
+            navWrite.textContent="Vitamine -"
+    }
+    else{
+        navWrite.textContent="Le vitamine+"
+        //delete table
+        const existingTable = document.getElementById("table_1")
+        if(existingTable){
+            existingTable.remove()
+        }
+        const existingBtDelete = document.getElementById("delete_bt")
+        if(existingBtDelete){
+            existingBtDelete.remove()
+            return;
+        }
+    }
+    
+    
+
+})
+
